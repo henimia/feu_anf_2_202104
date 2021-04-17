c5481328cf8197a0c656ceaa3fe9879141fbf308
MichaelZett <michael2.zoeller@gmail.com>
2021-04-17 11:24:07 +0200

    methods and static

--- fizzbuzz/src/fizzbuzz/FizzBuzzGame.java
+++ fizzbuzz/src/fizzbuzz/FizzBuzzGame.java
@@ -1,28 +1,57 @@
 package fizzbuzz;
 
 class FizzBuzzGame {
-	private int number;
+	private static final int BUZZ_NUMBER = 5;
+	private static final int FIZZ_NUMBER = 3;
+	private static final String BUZZ = "Buzz";
+	private static final String FIZZ = "Fizz";
+	private static final String FIZZ_BUZZ = FIZZ + BUZZ;
+
+	private static int gameCounter;
+
+	private int endNumber;
 
 	public FizzBuzzGame(int number) {
 		super();
-		this.number = number;
+		FizzBuzzGame.gameCounter++;
+		this.endNumber = number;
 	}
 
 	void play() {
-		for (int i = 1; i <= number; i++) {
-			if (i % 3 == 0 && i % 5 == 0) {
-				System.out.print("FizzBuzz");
-			} else if (i % 3 == 0) {
-				System.out.print("Fizz");
-			} else if (i % 5 == 0) {
-				System.out.print("Buzz");
-			} else {
-				System.out.print(i);
-			}
-			if (i < number) {
+		for (int i = 1; i <= endNumber; i++) {
+			doFizzBuzzForSingleNumber(i);
+			if (i < endNumber) {
 				System.out.print(", ");
 			}
 		}
+	}
+
+	private static void doFizzBuzzForSingleNumber(int number) {
+		if (isFizzBuzz(number)) {
+			System.out.print(FIZZ_BUZZ);
+		} else if (isFizz(number)) {
+			System.out.print(FIZZ);
+		} else if (isBuzz(number)) {
+			System.out.print(BUZZ);
+		} else {
+			System.out.print(number);
+		}
+	}
+
+	private static boolean isBuzz(int number) {
+		return number % BUZZ_NUMBER == 0;
+	}
+
+	private static boolean isFizz(int number) {
+		return number % FIZZ_NUMBER == 0;
+	}
+
+	private static boolean isFizzBuzz(int number) {
+		return isFizz(number) && isBuzz(number);
+	}
+
+	public static int getGameCounter() {
+		return gameCounter;
 	}
 
 }
--- fizzbuzz/src/fizzbuzz/FizzBuzzStarter.java
+++ fizzbuzz/src/fizzbuzz/FizzBuzzStarter.java
@@ -5,6 +5,11 @@ public class FizzBuzzStarter {
 	public static void main(String[] args) {
 		FizzBuzzGame fizzBuzzGame = new FizzBuzzGame(16);
 		fizzBuzzGame.play();
+
+		FizzBuzzGame fizzBuzzGame2 = new FizzBuzzGame(32);
+		fizzBuzzGame2.play();
+		System.out.println();
+		System.out.println("Anzahl gespielter FizzBuzz-Spiele: " + FizzBuzzGame.getGameCounter());
 	}
 
 }
