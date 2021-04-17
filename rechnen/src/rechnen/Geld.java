507f96fafcf6c993ca492df60c575cc5b0649223
MichaelZett <michael2.zoeller@gmail.com>
2021-04-17 11:31:55 +0200

    add initial Geld

---
+++ rechnen/src/rechnen/Geld.java
@@ -0,0 +1,9 @@
+package rechnen;
+
+public class Geld {
+	// geld hat einen Betrag (double) und eine Währung (String)
+	// Geld soll mit anderem Geld verrechnbar sein wenn die Währung passt
+	// 2 Methoden: Geld add(Geld geld) und Geld substract(Geld geld)
+	// es gibt kein negatives Geld -> exception (IllegalArgumentException)
+	// beim Versuch mit anderer Währung zun rechnen ebenso
+}
